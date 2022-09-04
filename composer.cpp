2d99962bd36642143724c873a4fb0e638397eeb8
Pavel Kirienko <pavel.kirienko@gmail.com>
2022-09-04 22:08:40 +0300

    Add fixed point to composer

--- composer.cpp
+++ composer.cpp
@@ -12,6 +12,8 @@ int main()
         .uavcan_file_name         = {},
         .stay_in_bootloader       = true,
     };
+    const std::array<std::uint8_t, 8> fixed_point_name{0xA2, 0x35, 0xD2, 0x12, 0xAB, 0x45, 0x27, 0x02};
+    std::memcpy(obj.uavcan_file_name.data(), fixed_point_name.data(), fixed_point_name.size());
     const auto buffer = composeWithLeadingCRC(obj);
     std::cout.write(reinterpret_cast<const char*>(buffer.data()), buffer.size());
     return 0;
